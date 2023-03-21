3f487af965fdfea4a077cd84912d5dcfa77ef12f
NastiaShulgina <75643824+NastiaShulgina@users.noreply.github.com>
2023-03-21 17:06:00 +0200

    2.20*: Data for countries, step3

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -1,4 +1,28 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const CountryInfo = ({ country }) => {
+
+    const [weather, setWeather] = useState({})
+
+    const apiKey = process.env.REACT_APP_API_KEY
+
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${apiKey}&units=metric`
+
+    useEffect(() => {
+        const request = axios.get(weatherUrl)
+        request
+            .then(response => response.data)
+            .then(responseData => {
+                setWeather({
+                    temperature: responseData.main.temp,
+                    iconSrc: `https://openweathermap.org/img/wn/${responseData.weather[0].icon}@2x.png`,
+                    iconAlt: responseData.weather[0].description,
+                    windSpeed: responseData.wind.speed
+                })
+            })
+    }, [weatherUrl])
+
     return (
         <div>
             <h2>{country.name.common}</h2>
@@ -12,7 +36,10 @@ const CountryInfo = ({ country }) => {
                     })}
                 </ul>
             </div>
-            <img src={country.flags.png} alt={country.flags.src}></img>
+            <img src={country.flags.png} alt={country.flags.src} className='flag' />
+            <div>temperature {weather.temperature} Celcius</div>
+            <img src={weather.iconSrc} alt={weather.iconAlt} />
+            <div>wind {weather.windSpeed} m/s</div><br />
         </div>
     );
 }
